2288ec48c9f53ca6af41f34c4f0ed913b03b69c1
Enrique Lopez <elop22@gmail.com>
2020-07-19 20:47:08 -0600

    added html, css, question.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var countdownEL = document.querySelector('countdown');
+var questionsEL = document.querySelector('question');
+var button1EL = document.querySelector('btn1');
+var button2EL = document.querySelector('btn2');
+var button3EL = document.querySelector('btn3');
+var button4EL = document.querySelector('btn4');
+var startEL = document.querySelector('start-btn btn');
+var nextEL = document.querySelector('next-btn btn hide');
+
+var score;
+var time;
+var quizTime;
+
+
---
+++ question.js
@@ -0,0 +1,28 @@
+var questionArr = [
+  {
+    question: 'Which of the following is NOT a JavaScript data type',
+    answers: ["string", "number", "boolean", "meta"],
+    correct: 'meta'
+  },
+  {
+    question: 'Which built in method returns the string value to lower case?',
+    answers: ['toLowerCase', 'changeCase(lower', 'toLower', 'none of the above'],
+    correct: 'toLowerCase'
+  },
+  {question: "Which of the following is NOT part of a for loop",
+    answers: ["variable", "condition", "increment", "boolean"],
+    correct: "boolean"
+  },
+  {question: "JavaScript and Java...",
+    answers: ["Are the Same", "Are built off of eachother", "were created by the same person", "Are different programming languages"],
+    correct: "Are different programming languages"
+  },
+  {question: "Which of the following is NOT a JavaScript framework?",
+    answers: ["Node.js", "React.js", "Go.js", "Vue.js"],
+    correct: "Go.js"
+  },
+  {question: "How do you write an IF statement in JavaScript?",
+    answers: ["if = 5 then","if i == 5 then", "if i = 5", "if(i==5)"],
+    correct: "if(i==5)"
+  },
+]
